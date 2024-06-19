637facae5c091da4ae234f7f26333e3b79b861be
Ananya <ananyaagrawal073@gmail.com>
2024-06-19 23:00:17 +0530

    commit
    commit

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
